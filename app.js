48152ea3cd8b37dcd632c8dd6e9e8776543fdfbf
ritikaa-21 <ritika.kaushal1997@gmail.com>
2021-08-19 01:18:54 -0400

    msg

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require("express");
+const excelToJson = require('convert-excel-to-json');
+
 var app = express();
+
+ 
+const result = excelToJson({
+    sourceFile: 'X Y Crematoirums.xlsx'
+});
+
+console.log(result);
+
+
 
 app.listen(8080, ()=>{
     console.log("Listening at 8080");
