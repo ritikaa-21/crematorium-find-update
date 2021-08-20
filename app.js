a0cc2cb40d34899a857c02ccbeccc248f07d181a
ritikaa-21 <ritika.kaushal1997@gmail.com>
2021-08-20 11:56:37 -0400

    msg

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ function findClosestCrematorium(x, y) {
 
     //get updated json
     const result = getUpdatedExcel();
-
+    let found = false;
     let leastDist =
     {
         "name": result["Sheet1"][0].name,
@@ -74,11 +74,10 @@ function findClosestCrematorium(x, y) {
         "Y": result["Sheet1"][0].Y
     }
 
-
     //this method is optimized in a way that the loop exits if in a corner case, an exact match is found with the user entered coordinates, 
     //since there can't be a distance shorter than the exact coordinates
-
-    for (let i = 0; i < result["Sheet1"].length && (leastDist.X !== x && leastDist.Y !== y); i++) {
+// && (leastDist.X !== x || leastDist.Y !== y)
+    for (let i = 0; i < result["Sheet1"].length && (leastDist.X !== x || leastDist.Y !== y); i++) {
         if (Math.hypot(x - result["Sheet1"][i].X, y - result["Sheet1"][i].Y) < Math.hypot(x - leastDist.X, y - leastDist.Y)) {
             leastDist = result["Sheet1"][i];
         }
